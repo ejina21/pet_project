b92f3f5e270148060844d958c4f0e48e1db6a75b
nikita <kitan98@mail.ru>
2023-09-10 22:11:35 +0300

    ref: переместил тесты и переименовал

--- app/tests/integration_tests/test_i_bookings.py
+++ app/tests/integration_tests/test_i_bookings.py
@@ -1,5 +1,7 @@
 import pytest
 from httpx import AsyncClient
+from datetime import datetime
+from app.bookings.dao import BookingDAO
 
 
 @pytest.mark.parametrize("room_id,date_from,date_to,booked_rooms,status_code", [
@@ -44,3 +46,37 @@ async def test_get_and_delete_booking(authenticated_ac: AsyncClient):
 
     response = await authenticated_ac.get("/api/v1/bookings")
     assert len(response.json()) == 0
+
+
+@pytest.mark.parametrize("user_id, room_id", [
+    (2, 2),
+    (2, 3),
+    (1, 4),
+    (1, 4),
+])
+async def test_booking_crud(user_id, room_id):
+    # Добавление брони
+    new_booking = await BookingDAO.add_row(
+        user_id=user_id,
+        room_id=room_id,
+        date_from=datetime.strptime("2023-07-10", "%Y-%m-%d"),
+        date_to=datetime.strptime("2023-07-24", "%Y-%m-%d"),
+    )
+
+    assert new_booking["user_id"] == user_id
+    assert new_booking["room_id"] == room_id
+
+    # Проверка добавления брони
+    new_booking = await BookingDAO.find_one_or_none(id=new_booking.id)
+
+    assert new_booking is not None
+
+    # Удаление брони
+    await BookingDAO.delete(
+        id=new_booking["id"],
+        user_id=user_id,
+    )
+
+    # Проверка удаления брони
+    deleted_booking = await BookingDAO.find_one_or_none(id=new_booking["id"])
+    assert deleted_booking is None
